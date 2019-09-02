2860519070a7cdf61db9932b2e390b0611b65863
uzair iqbal <uzair.qq@outlook.com>
2019-09-02 18:20:35 +0500

    Convention Based Routing with multiple routes and projec structure

--- ToDoApp/ToDoApp/Startup.cs
+++ ToDoApp/ToDoApp/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ToDoApp
 {
@@ -15,20 +17,45 @@ namespace ToDoApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services. !!important to use mvc
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //this is default routes convention based routing
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id:int?}",
+                    defaults: new
+                    {
+                        controller = "Home",
+                        action = "Index"
+                    });
+
+                //more routing
+                routes.MapRoute(
+                name: "todo",
+                template: "convention/{action}/{id:int?}",
+                defaults: new { controller = "Todo", action = "Index" }
+                );
+
             });
+
         }
     }
 }
+
