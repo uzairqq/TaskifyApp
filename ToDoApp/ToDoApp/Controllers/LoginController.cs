5183fda6d7a3b3ed72b77b6854cf321531ffd088
uzair <uzair.qq@outlook.com>
2019-09-19 07:42:33 +0500

    Created LoginController,with Login Modal and use renderSection and SectionScripts for rendering js

---
+++ ToDoApp/ToDoApp/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp.Controllers
+{
+    public class LoginController:Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
