7eda137392171ccd9f399d1261fad806bb7e64be
Uzair Iqbal <uzair.qq@outlook.com>
2023-10-16 07:31:14 -0700

    Task Model. Task Db Context

---
+++ Taskify API/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskify_API.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(250)]
+        public string Description { get; set; }
+        public DateTime CreatedDateAndTime { get; set; }
+        public DateTime DueDateAndTime { get; set; }
+        public string Priority { get; set; }
+        public bool IsComplete { get; set; } = false;
+    }
+}
