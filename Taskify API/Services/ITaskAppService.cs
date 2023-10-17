82c0cef3dbb210b0727cfb8173f4e1e638b615d3
Uzair Iqbal <uzair.qq@outlook.com>
2023-10-17 03:20:07 -0700

    Created Services For Logic.

---
+++ Taskify API/Services/ITaskAppService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Taskify_API.Dto;
+
+namespace Taskify_API.Services
+{
+    public interface ITaskAppService
+    {
+        Task<ResponseMessageDto> PostAsync(CreateUpdateTaskDto input);
+        Task<ResponseMessageDto> PutAsync(int id, CreateUpdateTaskDto input);
+        Task<List<GetTaskListDto>> GetListAsync();
+        Task<GetTaskDto> GetByIdAsync(int id);
+        Task<ResponseMessageDto> DeleteAsync(int id);
+    }
+}
