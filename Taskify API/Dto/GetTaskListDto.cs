d367fa601234fe859a704d64ac7fa8cb1a4ce011
Uzair Iqbal <uzair.qq@outlook.com>
2023-10-17 03:19:44 -0700

    Created Dtos And Response Message Dto

---
+++ Taskify API/Dto/GetTaskListDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskify_API.Dto
+{
+    public class GetTaskListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDateAndTime { get; set; }
+        public DateTime DueDateAndTime { get; set; }
+        public string Priority { get; set; }
+        public bool IsComplete { get; set; } = false;
+    }
+}
---
+++ Taskify API/Dto/ResponseMessageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Taskify_API.Dto
+{
+    public class ResponseMessageDto
+    {
+        public int Id { get; set; }
+        public string SuccessMessage { get; set; }
+        public bool IsSuccess { get; set; }
+        public string FailureMessage { get; set; }
+        public bool IsFailed { get; set; }
+    }
+}
