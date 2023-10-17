d29687b9205121ebb504362cf170da6f0495c7c0
Uzair Iqbal <uzair.qq@outlook.com>
2023-10-17 03:20:44 -0700

    Register Services and Repository

--- Taskify API/Program.cs
+++ Taskify API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Taskify_API.Models;
+using Taskify_API.Repository;
+using Taskify_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Business Servicess Register
+builder.Services.AddScoped<ITaskAppService, TaskAppService>();
+
+//Business Repository Register
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+
+
 
 //Connection string
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
