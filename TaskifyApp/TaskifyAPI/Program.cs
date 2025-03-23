d1625e6cdd1c2b3d1732a63b17a0ffe1e348adb9
Uzair Iqbal <uzair.qq@outlook.com>
2025-03-24 04:11:15 +0500

    Global Error Handling

--- TaskifyApp/TaskifyAPI/Program.cs
+++ TaskifyApp/TaskifyAPI/Program.cs
@@ -6,6 +6,7 @@ using TaskifyAPI.Services.Implementation;
 using TaskifyAPI.Services.Interface;
 using TaskifyAPI.Repositories.Interface;
 using TaskifyAPI.Repositories.Implementation;
+using TaskifyAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,12 @@ builder.Services.AddDbContext<TaskifyDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("TaskifyDbContext"));
 });
+
+
+// Add built-in logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 builder.Services.AddScoped<ITodoServices, TodoService>();
 builder.Services.AddScoped<ITodoRepository, TodoRepository>();
@@ -32,7 +39,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskifyAPI v1"));
 }
-
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
