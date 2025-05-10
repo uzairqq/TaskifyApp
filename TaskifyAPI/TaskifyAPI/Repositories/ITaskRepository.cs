74e80068b02a7f8d1ebaf753c4cc1797275e5650
Uzair Iqbal <uzair.qq@outlook.com>
2025-05-10 12:38:58 +0500

    Implemented TaskRepository with CRUD operations

---
+++ TaskifyAPI/TaskifyAPI/Repositories/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskifyAPI.Models;
+
+namespace TaskifyAPI.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync();
+        Task<TaskItem?> GetByIdAsync(int id);
+        Task<TaskItem> AddAsync(TaskItem taskItem);
+        Task<TaskItem?> UpdateAsync(TaskItem taskItem);
+        Task<bool> DeleteAsync(int id);
+    }
+}
