27b7dc24281285147e0e0b4b2d475781ffaef506
Uzair Iqbal <uzair.qq@outlook.com>
2025-05-15 13:24:29 +0500

    Changed a property with title with Name.

--- TaskifyAPI/TaskifyAPI/Models/TaskItem.cs
+++ TaskifyAPI/TaskifyAPI/Models/TaskItem.cs
@@ -3,7 +3,7 @@
     public class TaskItem
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string? Description { get; set; }
         public bool IsCompleted { get; set; }
         public DateTime CreatedAt { get; set; }= DateTime.Now;
