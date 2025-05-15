573d5a7bc70d9c3b17071385bace116c84ef1a33
Uzair Iqbal <uzair.qq@outlook.com>
2025-05-15 13:26:10 +0500

    Using InMemory Database For Testing.

--- TaskifyAPI/TaskifyAPI/Program.cs
+++ TaskifyAPI/TaskifyAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using TaskifyAPI.Data;
 using TaskifyAPI.Repositories;
 using TaskifyAPI.Services;
 
@@ -7,8 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("TaskifyDb"));
+
 builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
