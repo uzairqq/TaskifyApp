2beb68ed0efa205f406ad3562f011ada6a076dfc
Uzair Iqbal <uzair.qq@outlook.com>
2025-05-15 13:25:04 +0500

    Created Whole CRUD Functionality Controller.

---
+++ TaskifyAPI/TaskifyAPI/Controllers/TaskController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TaskifyAPI.Models;
+using TaskifyAPI.Services;
+
+namespace TaskifyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var tasks = await _taskService.GetAllAsync();
+            return Ok(tasks);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var task = await _taskService.GetByIdAsync(id);
+            if (task == null) return NotFound();
+            return Ok(task);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(TaskItem taskItem)
+        {
+            var createdTask = await _taskService.AddAsync(taskItem);
+            return CreatedAtAction(nameof(GetById), new { id = createdTask.Id }, createdTask);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(int id, TaskItem taskItem)
+        {
+            if (id != taskItem.Id)
+                return BadRequest();
+
+            var updated = await _taskService.UpdateAsync(taskItem);
+            if (updated == null)
+                return NotFound();
+
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var success = await _taskService.DeleteAsync(id);
+            if (!success)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
