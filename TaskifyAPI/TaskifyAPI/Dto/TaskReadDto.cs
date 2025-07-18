2b707c0ddf9d9fd5a877b3bbd490b18f91513606
Uzair Iqbal <uzair.qq@outlook.com>
2025-07-18 11:59:29 +0500

    Removed All Things

--- TaskifyAPI/TaskifyAPI/Dto/TaskReadDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TaskifyAPI.Dto
-{
-    public class TaskReadDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public bool IsCompleted { get; set; }
-    }
-}
