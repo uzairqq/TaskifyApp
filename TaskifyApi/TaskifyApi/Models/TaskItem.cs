49e3a7dfd351bf0d9c2b2d4314a5a19cb940ea07
Uzair Iqbal <uzair.qq@outlook.com>
2025-08-25 11:45:23 +0500

    added blank Taskify solution

--- TaskifyApi/TaskifyApi/Models/TaskItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TaskifyApi.Models
-{
-    public class TaskItem
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public bool IsCompleted { get; set; }=false;
-    }
-}
