4d875c3093c59e448b8af62884cc45a8c0b911e0
Uzair Iqbal <uzair.qq@outlook.com>
2025-12-22 14:12:17 +0500

    Added In Memory Post and Get All.

---
+++ backend/Taskify/Taskify.Api/Controllers/TasksController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using Taskify.Api.Models;
+
+namespace Taskify.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        static List<TaskItem> taskItems = new List<TaskItem>()
+        {
+            new TaskItem
+            {
+                Id=1,
+                Title="Wash Car",
+                Status=false
+
+            }
+        };
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(taskItems);
+        }
+        [HttpPost]
+        public IActionResult Post(TaskItem taskItem)
+        {
+            var newId = (taskItems.Count == 0) ? 1 : taskItems.Max(x => x.Id) + 1;
+
+            var task = new TaskItem
+            {
+                Id = newId,
+                Title = taskItem.Title,
+                Status = taskItem.Status
+
+            };
+            taskItems.Add(task);
+            return Ok(task);
+        }
+
+    }
+}
---
+++ backend/Taskify/Taskify.Api/Models/TaskItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Taskify.Api.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Status { get; set; }
+    }
+}
